e0ecc2b4fbefe247ab49a69fec186a955f345761
user <cvc575@gmail.com>
2017-01-26 20:33:02 +0300

    Check git on linux

--- Game/src/main/java/ru/simpleapps/game/MainActivity.java
+++ Game/src/main/java/ru/simpleapps/game/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends Activity {
 
     GameView gameView;
 
+    // Check git on linux
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
